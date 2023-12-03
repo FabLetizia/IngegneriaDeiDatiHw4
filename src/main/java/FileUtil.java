931743443b77572f176a1c8dec40f019a8d1d899
Riccardo De Cesaris <ric.dc@hotmail.it>
2023-12-03 13:45:08 +0100

    Aggiornamento FileUtil.java
    Miglioramento dell'estrazione del campione per il Benchmark: quando estraggo un file random dalla directory dei documenti non devo mai prendere uno dei "parsing files"

--- src/main/java/FileUtil.java
+++ src/main/java/FileUtil.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class FileUtil {
 
     public static List<File> getRandomSampleFromDirectory(String directoryPath, int sampleSize) {
-        List<File> allFiles = getAllFilesInDirectory(directoryPath);
+        List<File> allFiles = getAllXMLFilesInDirectory(directoryPath);
 
         if (sampleSize >= allFiles.size()) {
             return allFiles; // Se il campione richiesto è maggiore o uguale al numero totale di file, restituisci tutti i file.
@@ -16,18 +16,22 @@ public class FileUtil {
         // Usa un seme casuale per ottenere risultati diversi ad ogni esecuzione
         long seed = System.nanoTime();
         Collections.shuffle(allFiles, new Random(seed));
-
+        
         // Estrai il campione
         return allFiles.subList(0, sampleSize);
     }
 
-    private static List<File> getAllFilesInDirectory(String directoryPath) {
+    private static List<File> getAllXMLFilesInDirectory(String directoryPath) {
         List<File> files = new ArrayList<>();
         File directory = new File(directoryPath);
         File[] fileList = directory.listFiles();
-        if (fileList != null) {
-            Collections.addAll(files, fileList);
+        
+        for (File file: fileList) {
+        	// I "parsing files" non devono far parte del campione
+        	if (file.getName().contains(".xml"))		
+        		files.add(file);
         }
+        
         return files;
     }
 }
